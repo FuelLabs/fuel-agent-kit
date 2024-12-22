b1735903e95196c363d8878ab5851aa10a76dc56
Deepak <deepakkomma@gmail.com>
2024-12-22 13:27:40 +0530

    added executeLangraph to the FuelAgentClass

--- src/FuelAgent.ts
+++ src/FuelAgent.ts
@@ -2,6 +2,8 @@ import { agentExector } from './agent.js';
 import { addLiquidity, type AddLiquidityParams } from './mira/addLiquidity.js';
 import { swapExactInput, type SwapExactInputParams } from './mira/swap.js';
 import { borrowAsset, type BorrowAssetParams } from './swaylend/borrow.js';
+import { graph } from './langraph/index.js';
+import { HumanMessage } from '@langchain/core/messages';
 import {
   supplyCollateral,
   type SupplyCollateralParams,
@@ -20,6 +22,14 @@ export class FuelAgent {
     if (!process.env.FUEL_WALLET_PRIVATE_KEY) {
       throw new Error('FUEL_WALLET_PRIVATE_KEY is not set');
     }
+  }
+
+  async exceuteLanggraph(input: string) {
+    const response = await graph.invoke({
+      messages: [new HumanMessage(input)],
+    });
+
+    return response;
   }
 
   async execute(input: string) {
